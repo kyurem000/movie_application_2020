0cfe44f19b05d02c7e9acf45624c7ce36dec408e
kyurem000 <hanapsk61@knou.ac.kr>
2020-07-20 07:36:50 +0900

    p.78까지 학습완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({fav}) {
+  return <h1> i like {fav} </h1>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+  <div> 
+    <h1> Hello </h1> 
+    <Food fav="kimchi_78" /> 
+    <Food fav="ramen" />
+    <Food fav="samiopsal" />
+    <Food fav="chunkumi" />
+  </div>
   );
 }
 
